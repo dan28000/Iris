12d301d5262cd0ce6e02a185e956e4830d5cc7b4
dan28000 <84990628+dan28000@users.noreply.github.com>
2025-10-27 19:57:13 +0100

    Update MultiBurst.java

--- core/src/main/java/com/volmit/iris/util/parallel/MultiBurst.java
+++ core/src/main/java/com/volmit/iris/util/parallel/MultiBurst.java
@@ -75,7 +75,7 @@ public class MultiBurst implements ExecutorService {
                             return worker;
                         }
                     },
-                    (t, e) -> e.printStackTrace(), true);
+                    (t, e) -> e.printStackTrace(), true, 1, Runtime.getRuntime().availableProcessors(), 1, null, 60_000L, TimeUnit.MILLISECONDS);
         }
 
         return service;
